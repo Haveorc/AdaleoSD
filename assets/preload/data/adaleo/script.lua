65a4a9087f2093bf66689ad16ae4a9af95a3faf6
Max <39416497+Haveorc@users.noreply.github.com>
2022-02-23 23:35:36 +0000

    The REAL last thing

--- assets/preload/data/adaleo/script.lua
+++ assets/preload/data/adaleo/script.lua
@@ -1,3 +1,20 @@
+local allowCountdown = false
+function onStartCountdown()
+	if not allowCountdown and isStoryMode and not seenCutscene then
+		setProperty('inCutscene', true);
+		runTimer('startDialogue', 0.8);
+		allowCountdown = true;
+		return Function_Stop;
+	end
+	return Function_Continue;
+end
+
+function onTimerCompleted(tag, loops, loopsLeft)
+	if tag == 'startDialogue' then
+		startDialogue('dialogue', 'arrival');
+	end
+end
+
 function onBeatHit()
 	if curBeat == 164 then
 	cameraFlash(game, 'FFFFFF', 0.36, true)
--- assets/preload/data/eternal/script.lua
+++ assets/preload/data/eternal/script.lua
@@ -6,6 +6,23 @@ function onCreate()
 	setProperty('altFade.alpha', 0.0000001);
 end
 
+local allowCountdown = false
+function onStartCountdown()
+	if not allowCountdown and isStoryMode and not seenCutscene then
+		setProperty('inCutscene', true);
+		runTimer('startDialogue', 0.8);
+		allowCountdown = true;
+		return Function_Stop;
+	end
+	return Function_Continue;
+end
+
+function onTimerCompleted(tag, loops, loopsLeft)
+	if tag == 'startDialogue' then
+		startDialogue('dialogue', 'wondersoftheentrance');
+	end
+end
+
 function onBeatHit()
 	if curBeat == 96 or curBeat == 160 or curBeat == 192 or curBeat == 224 or curBeat == 256 or curBeat == 288 then
 	cameraFlash(game, 'FFFFFF', 0.36, true)
